6f58c5a1b48792ffc3a499d4d2215301874e6afe
Pradeep7007 <156215080+Pradeep7007@users.noreply.github.com>
2024-09-26 15:43:35 +0530

    Create second largest.c

---
+++ second largest.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+  int a[n];
+  for(int i=0;i<n;i++){
+      scanf("%d",&a[i]);
+  }
+  int length = sizeof(a)/sizeof(a[0]);
+
+  int max1;
+  int max2;
+
+  if (a[0] > a[1])
+  {
+    max1 = a[0];
+    max2 = a[1];
+  }
+  else
+  {
+    max1 = a[1];
+    max2 = a[0];
+  }
+  for (int i = 2; i < length; i++)
+  {
+    if (a[i] > max1)
+    {
+      max2 = max1;
+      max1 = a[i];
+    }
+    else if (a[i] > max2 && a[i] < max1)
+    {
+      max2 = a[i];
+    }
+  }
+  printf("second highest: %d\n", max2);
+  return 0;
+}
